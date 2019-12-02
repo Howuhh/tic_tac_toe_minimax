5ee257ba612963f1f67acb7470d9d4d1f84ff116
Howuhh <hsehowuhh@gmail.com>
2019-12-03 00:57:39 +0300

    add board state & win-tie check

---
+++ start_game.py
@@ -0,0 +1,6 @@
+from tic_tac.game import Game
+
+if __name__ == "__main__":
+    new_game = Game()
+
+    new_game.start_game()
---
+++ tic_tac/game.py
@@ -0,0 +1,10 @@
+from .board import Board
+
+class Game:
+    def __init__(self):
+        pass
+
+    def start_game(self):
+        board = Board()
+        
+        board.print_board()
